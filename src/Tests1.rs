a25abad0293de4d73b68530e15fd2424b75ecf6b
Chris Clark <github@chrisclark.net>
2024-08-10 10:59:10 -0400

    Adding test examples

---
+++ src/Tests1.rs
@@ -0,0 +1,18 @@
+pub fn is_divisible_by_three(num: i32) -> bool {
+    num % 3 == 0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_true_when_divisible_by_three() {
+        assert!(is_divisible_by_three(9));
+    }
+
+    #[test]
+    fn is_false_when_not_divisible_by_three() {
+        assert!(!is_divisible_by_three(8));
+    }
+}
---
+++ src/Tests2.rs
@@ -0,0 +1,27 @@
+// Problem 2: Add a suitable attribute so that the test passes.
+
+
+fn find_max(nums: &[i32]) -> i32 {
+    if nums.is_empty() {
+        panic!("Empty number list");
+    }
+    let mut max = nums[0];
+    for &num in nums {
+        if num > max {
+            max = num;
+        }
+    }
+    max
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[should_panic] // <-------
+    #[test]  
+    fn panics_on_empty_list() {
+        let nums = [];
+        let _max = find_max(&nums);
+    }
+}
